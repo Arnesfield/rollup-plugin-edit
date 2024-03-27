97c1f5c33310e93dab307538ff7f19a6b3fd0b5d
Jefferson Rylee <rylee.jeff385@gmail.com>
2024-03-27 15:42:33 +0800

    Remove umd and sourcemaps from build

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,6 @@
 import _eslint from '@rollup/plugin-eslint';
 import _typescript from '@rollup/plugin-typescript';
-import { OutputOptions, RollupOptions } from 'rollup';
+import { RollupOptions } from 'rollup';
 import dts from 'rollup-plugin-dts';
 import esbuild, { Options as EsbuildOptions } from 'rollup-plugin-esbuild';
 import outputSize from 'rollup-plugin-output-size';
@@ -10,25 +10,11 @@ import pkg from './package.json' with { type: 'json' };
 const eslint = _eslint as unknown as typeof _eslint.default;
 const typescript = _typescript as unknown as typeof _typescript.default;
 
-// skip sourcemap and umd unless production
 const PROD = process.env.NODE_ENV !== 'development';
-
-const name = pkg.name.slice(pkg.name.lastIndexOf('/') + 1);
 const input = 'src/index.ts';
-const inputUmd = 'src/index.umd.ts';
 
 function build(options: EsbuildOptions = {}) {
   return esbuild({ target: 'esnext', ...options });
-}
-
-function umd(options: Partial<OutputOptions>): OutputOptions {
-  return {
-    name,
-    format: 'umd',
-    exports: 'default',
-    sourcemap: PROD,
-    ...options
-  };
 }
 
 function defineConfig(options: (false | RollupOptions)[]) {
@@ -39,20 +25,10 @@ export default defineConfig([
   {
     input,
     output: [
-      { file: pkg.main, format: 'cjs', exports: 'named', sourcemap: PROD },
-      { file: pkg.module, format: 'esm', exports: 'named', sourcemap: PROD }
+      { file: pkg.main, format: 'cjs', exports: 'named' },
+      { file: pkg.module, format: 'esm', exports: 'named' }
     ],
     plugins: [build(), outputSize()]
-  },
-  PROD && {
-    input: inputUmd,
-    output: umd({ file: pkg.unpkg.replace(/\.min\.js$/, '.js') }),
-    plugins: [build(), outputSize()]
-  },
-  PROD && {
-    input: inputUmd,
-    output: umd({ file: pkg.unpkg }),
-    plugins: [build({ minify: true }), outputSize()]
   },
   {
     input,
